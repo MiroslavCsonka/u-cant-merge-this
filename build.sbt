67f87c0883aaf470d82cea8e189dc11f8f3847c1
Miroslav Csonka <miroslav.csonka@gmail.com>
2016-05-02 14:52:34 +0100

    Add specs for decision engine

--- build.sbt
+++ build.sbt
@@ -12,6 +12,7 @@ libraryDependencies ++= Seq(
   "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
   "org.scalaz" %% "scalaz-core" % "7.2.2",
   "org.scalaj" %% "scalaj-http" % "2.3.0",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % "test",
   ws
 )
 
---
+++ test/app/models/DecisionEngineSpec.scala
@@ -0,0 +1,37 @@
+package app.models
+
+import models.{ApprovalsConfig, DecisionEngine, Comment}
+import org.scalatestplus.play._
+
+class DecisionEngineSpec extends PlaySpec {
+  val noComments = List()
+  val positiveComment = Comment("nice", "MiroslavCsonka")
+
+  def decision(comments: List[Comment], config: ApprovalsConfig) = DecisionEngine.enoughReviews(comments, config)
+
+  "DecisionEngine" must {
+    "approve" when {
+      "minimum reviews are 0" in {
+        decision(noComments, ApprovalsConfig(List(), 0)) mustBe true
+      }
+
+      "someone commented and" when {
+        "no patterns are supplied" in {
+          decision(List(positiveComment), ApprovalsConfig(List(), 1)) mustBe true
+        }
+      }
+    }
+
+    "decline" when {
+      "no one commented" in {
+        decision(noComments, ApprovalsConfig(List(), 1)) mustBe false
+      }
+
+      "someone commented and" when {
+        "none of patterns match" in {
+          decision(List(positiveComment), ApprovalsConfig(List("^nope$"), 1)) mustBe false
+        }
+      }
+    }
+  }
+}
